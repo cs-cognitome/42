50e4f6cf7c877ff54442a68a2fd8a0eca6c2bcd6
Tanya Voronkova <tvoronko@z1r4p3.le-101.fr>
2019-07-24 19:32:29 +0200

    new check file

--- BSQ/ft_check_map.c
+++ BSQ/ft_check_map.c
@@ -6,11 +6,12 @@
 /*   By: tvoronko <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/22 19:04:41 by tvoronko     #+#   ##    ##    #+#       */
-/*   Updated: 2019/07/23 19:54:27 by tvoronko    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/07/24 19:31:58 by tvoronko    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -18,9 +19,9 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int ft_check_char(char c)
+int ft_check_char(char c, s_map map)
 {
-	if (c != 'o' && c != 'x' && c != '.')
+	if (c != map.wall && c != map.sqr && c != map.empty)
 	{
 		write(2, "map error\n", 10);
 		return (1);
@@ -29,14 +30,14 @@ int ft_check_char(char c)
 }
 
 
-int	ft_check_map(char *str)
+int	ft_check_map(char *str, s_map map)
 {
 	int i;
-	int n;	
+	int n;
 	int length; 
 	
 	i = 0;
-	n = 0;
+	length = 0;
 	while (str[i++] != '\n')
 		;
 	if (str[i] == '\0')
@@ -46,19 +47,18 @@ int	ft_check_map(char *str)
 	}
 	while (str[i] != '\n')
 	{
-		if (ft_check_char(str[i]))
+		if (ft_check_char(str[i], map))
 				return (1);
 		i++;
-		n++;
+		length++;
 	}
 	i++;
-	length = n;
 	while (str[i] != '\0')
 	{
 		n = 0;
 		while (str[i] != '\n')
 		{
-			if (ft_check_char(str[i]))
+			if (ft_check_char(str[i], map))
 					return (1);
 			i++;
 			n++;
@@ -70,13 +70,21 @@ int	ft_check_map(char *str)
 			return (1);
 		}
 	}
-	
+	if (ft_strlen(map.map) % map.nline != 0)
+	{
+		write(2, "map error\n", 10);
+		return (1);
+	}
 	return (0);
 }
 
 int  main()
 {
- 	char map[78] = "8.ox\n...o....\n........\n..o.....\n........\n.o......\n.....o..\n........\n..oo....\n\0";
+ 	char map[78] = "...o....\n........\n..o.....\n........\n.o......\n.....o..\n........\n..oo....\n\0";
+	int nline;
+	char empty = ".";
+	char wall = "o";
+	char sqr = "x";
 
  return(ft_check_map(map));
 }
